2182b767ca550ccbd6c1ffe468b6187c3b79ec8d
halil_kaya <hkaya@pragmasoft.com.tr>
2019-02-28 22:56:31 +0300

    changed analytics resolve order

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,12 +38,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `UA-135357675-1`,
-    //   },
-    // },
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
@@ -66,5 +60,11 @@ module.exports = {
       },
     },
     `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-135357675-1`,
+      },
+    },
   ],
 }
